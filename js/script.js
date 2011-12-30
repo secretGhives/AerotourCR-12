a9d1e2084ec89d07f3bd658d33477b0ecb787917
Hooker Juggernaut <secretgspot@gmail.com>
2011-12-30 02:28:14 -0600

    basic layout ready, home ready

--- js/script.js
+++ js/script.js
@@ -17,17 +17,29 @@ $(function() {
 	}
 
 	//load bootstrap stuff
-	$("[rel=pop]").popover( {offset: 10} ).click(function(e) { e.preventDefault() });
-	//$("[rel=tip]").twipsy({live: true});
-	$("[rel=roundabout]").roundabout();
 
+	if ($("[rel=pop]").length > 0){
+		$("[rel=pop]").popover( {offset: 10} ).click(function(e) { e.preventDefault() });
+	}
+	if ($("[rel=roundabout]").length > 0){
+		$("[rel=roundabout]").roundabout();
+	}
+		
+	if ($("[rel=tip]").length > 0){
+		$("[rel=tip]").twipsy({live: true});
+	}
+
+	if ($("table#sortTableExample").length > 0){
+		$("table#sortTableExample").tablesorter({ sortList: [[1,0]] });
+	}
+	
 	$("#toggleSitemap").toggle(
-	  function () {
-	    $("#sitemap").slideDown("fast");
-	  },
-	  function () {
-	    $("#sitemap").slideUp("fast");
-	  }
+		function () {
+			$("#sitemap").slideDown("fast");
+		},
+		function () {
+			$("#sitemap").slideUp("fast");
+		}
 	);
 
 	$(".sec-nav a").click(selectNav);
@@ -35,30 +47,30 @@ $(function() {
 });
 
 function	selectNav(e) {
-		e.preventDefault();
-		$("p.info").hide();
-		$("nav.active").removeClass("active");
+	e.preventDefault();
+	$("p.info").hide();
+	$("nav.active").removeClass("active");
 
-		$this = $(this).parent();
-		$this.addClass('active').find('p').fadeIn("slow");
+	$this = $(this).parent();
+	$this.addClass('active').find('p').fadeIn();
 }
 
-$(document).keydown(function (e) {
-	var keyCode = e.keyCode || e.which,
-			arrow = {left: 37, up: 38, right: 39, down: 40 };
+// $(document).keydown(function (e) {
+// 	var keyCode = e.keyCode || e.which,
+// 			arrow = {left: 37, up: 38, right: 39, down: 40 };
 
-	switch (keyCode) {
-		case arrow.left:
-			console.log('left!');
-		break;
-		case arrow.up:
-			console.log('up!');
-		break;
-		case arrow.right:
-			console.log('right!');
-		break;
-		case arrow.down:
-			console.log('down!');
-		break;
-	}
-});
+// 	switch (keyCode) {
+// 		case arrow.left:
+// 			console.log('left!');
+// 		break;
+// 		case arrow.up:
+// 			console.log('up!');
+// 		break;
+// 		case arrow.right:
+// 			console.log('right!');
+// 		break;
+// 		case arrow.down:
+// 			console.log('down!');
+// 		break;
+// 	}
+// });
